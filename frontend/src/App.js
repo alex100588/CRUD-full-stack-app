38478f8577f34d9bd03238abf3a8976f01ddccb9
alex100588 <alexandru_ionasc@yahoo.com>
2025-03-22 11:19:49 +0200

    create table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import User from './user/User';
 function App() {
   return (
     <div className="App">
-      <h1>react app</h1>
+      <h1 className='text-center mt-5'>CRUD App</h1>
       <User />
     </div>
   );
