57b2f5bfd2cc2287c4d5fbea942887ccdf8e5d19
alex100588 <alexandru_ionasc@yahoo.com>
2025-03-22 10:06:48 +0200

    update

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -48,3 +48,20 @@ export const getUserById = async(req, res)=>{
         res.status(500).json({ errorMessage: error.message })
     }
 }
+
+export const update = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const userExist = await User.findById(id);
+      if (!userExist) {
+        return res.status(404).json({ message: "User not found." });
+      }
+      const updatedData = await User.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      
+      res.status(200).json({ message: "User Updated successfully." });
+    } catch (error) {
+      res.status(500).json({ errorMessage: error.message });
+    }
+  };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { create, getAllUsers, getUserById } from "../controller/userController.js"
+import { create, getAllUsers, getUserById, update } from "../controller/userController.js"
 
 const route = express.Router()
 
@@ -9,5 +9,7 @@ route.post("/user", create)
 route.get('/users', getAllUsers)
 // extrage utilizator dupa id
 route.get('/user/:id', getUserById)
+// update
+route.put('/update/user/:id', update)
 
 export default route
