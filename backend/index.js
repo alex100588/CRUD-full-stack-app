41b9789b2de7e689bb40cd689893467bc62db688
alex100588 <alexandru_ionasc@yahoo.com>
2025-03-22 07:17:56 +0200

    insert data in  db

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import route from "./routes/userRoute.js";
 
 const app = express();
+app.get('/', (req, res)=>{
+  res.send("server is ready")
+})
+
+
+
 app.use(bodyParser.json());
 dotenv.config();
 
@@ -15,7 +22,9 @@ mongoose
   .then(() => {
     console.log("Db connected successfully");
     app.listen(PORT, () => {
-      console.log(`Server is running on port : ${PORT}`);
+      console.log(`Server is running on port http://localhost:${PORT}`);
     });
   })
   .catch((error) => console.log(error));
+
+  app.use("/api", route)
