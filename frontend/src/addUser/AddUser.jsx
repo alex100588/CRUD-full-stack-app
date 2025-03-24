aa584472d12a5e63e6e014fac1cda2a3f2374477
alex100588 <alexandru_ionasc@yahoo.com>
2025-03-24 14:23:53 +0200

    form

--- frontend/src/addUser/AddUser.jsx
+++ frontend/src/addUser/AddUser.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import './AddUser.css'
+import { Link } from 'react-router-dom'
 
 const AddUser = () => {
   return (
-    <div>Add User</div>
+    <div className='add-user'>
+        <Link to='/' className='btn btn-secondary'>
+            <i className="fa-solid fa-backward"> Back</i>
+        </Link>
+        <h3>Add new employee</h3>
+        <form className="add-user-form" >
+        <div className="input-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            autoComplete="off"
+            placeholder="Enter your Name"
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="email">E-mail:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            autoComplete="off"
+            placeholder="Enter your Email"
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="address">Address:</label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            autoComplete="off"
+            placeholder="Enter your Address"
+          />
+        </div>
+        <div className="inputGroup">
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
