886f4cc71f772f7186ec0a0147af82c4df55d32e
alex100588 <alexandru_ionasc@yahoo.com>
2025-03-24 16:03:12 +0200

    send data to server

--- frontend/src/addUser/AddUser.jsx
+++ frontend/src/addUser/AddUser.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
-import './AddUser.css'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import "./AddUser.css";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AddUser = () => {
+  const users = {
+    name: "",
+    email: "",
+    adress: "",
+  };
+  const [user, setUser] = useState(users);
+  const navigate = useNavigate();
+
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+
+    setUser({ ...user, [name]: value });
+  };
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:8000/api/user", user)
+      .then((response) => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className='add-user'>
-        <Link to='/' className='btn btn-secondary'>
-            <i className="fa-solid fa-backward"> Back</i>
-        </Link>
-        <h3>Add new employee</h3>
-        <form className="add-user-form" >
+    <div className="add-user">
+      <Link to="/" className="btn btn-secondary">
+        <i className="fa-solid fa-backward"> Back</i>
+      </Link>
+      <h3>Add new employee</h3>
+      <form className="add-user-form" onSubmit={submitForm}>
         <div className="input-group">
           <label htmlFor="name">Name:</label>
           <input
+            onChange={inputHandler}
             type="text"
             id="name"
             name="name"
@@ -23,6 +51,7 @@ const AddUser = () => {
         <div className="input-group">
           <label htmlFor="email">E-mail:</label>
           <input
+            onChange={inputHandler}
             type="email"
             id="email"
             name="email"
@@ -33,6 +62,7 @@ const AddUser = () => {
         <div className="input-group">
           <label htmlFor="address">Address:</label>
           <input
+            onChange={inputHandler}
             type="text"
             id="address"
             name="address"
@@ -40,14 +70,25 @@ const AddUser = () => {
             placeholder="Enter your Address"
           />
         </div>
-        <div className="inputGroup">
-          <button type="submit" class="btn btn-primary">
+        <div className="input-group">
+          <label htmlFor="address">Phone number:</label>
+          <input
+            onChange={inputHandler}
+            type="text"
+            id="address"
+            name="address"
+            autoComplete="off"
+            placeholder="Enter your Number"
+          />
+        </div>
+        <div className="input-group">
+          <button type="submit" className="btn btn-primary">
             Submit
           </button>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddUser
+export default AddUser;
