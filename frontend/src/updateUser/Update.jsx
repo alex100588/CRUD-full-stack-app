9ca14d41176c9fa5073c92845bf32e1bf6169cbe
alex100588 <alexandru_ionasc@yahoo.com>
2025-03-25 16:51:49 +0200

    url

--- frontend/src/updateUser/Update.jsx
+++ frontend/src/updateUser/Update.jsx
@@ -22,7 +22,7 @@ const Update = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:8000/api/user/${id}`)
+      .get(`https://crud-full-stack-app.vercel.app/user/${id}`)
       .then((response) => {
         setUser(response.data);
       })
@@ -34,7 +34,7 @@ const Update = () => {
   const submitForm = async (e) => {
     e.preventDefault();
     await axios
-      .put(`http://localhost:8000/api/update/user/${id}`, user)
+      .put(`https://crud-full-stack-app.vercel.app/user/${id}`, user)
       .then((response) => {
         toast.success(response.data.message, { positition: "top-center" });
         navigate("/");
